15a98b3cea34554025bfe1f7a76356050167a4b8
Jahangir Hussen <ahmedjahangirid@gmail.com>
2025-08-04 00:36:54 +0600

    added medicine planner to schedule medicine intake every 3 days

--- final.js
+++ final.js
@@ -4,3 +4,27 @@ var area = 800;
 
 var areaResult = area / 2;
 console.log(areaResult);
+
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 45000;
+//write your code here
+
+if (money >= 25000) {
+  console.log("Laptop");
+} else if (money >= 10000) {
+  console.log("Cycle");
+} else {
+  console.log("Chocolate");
+}
+
+/** Problem -03 ( Medicine Planner ) */
+var lastDay = 6;
+//write your code here
+
+for (var i = 1; i <= lastDay; i++) {
+  if (i % 3 === 0) {
+    console.log(i + " - medicine");
+  } else {
+    console.log(i + " - rest");
+  }
+}
